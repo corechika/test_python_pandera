612be5af38b26c4d6f98d8d707cff8dd7c8cdff5
corechika <rconctmi121341@gmail.com>
2023-07-19 22:49:58 +0900

    pandas だと Int64 じゃないと null を保持できないのも同様

--- UserSchema.py
+++ UserSchema.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 import pandera as pa
 from pandera.typing import Series, DateTime, Int, Float, String
 
@@ -5,7 +7,7 @@ class UserSchema(pa.DataFrameModel):
     # 各フィールドの定義と基本的なチェックを設定。
     id: Series[String] = pa.Field()  # idは文字列である
     age: Series[Float] = pa.Field(ge=18, le=60, coerce=True)  # 年齢は18以上60以下で、数値に強制変換する
-    income: Series[Int] = pa.Field(ge=1, le=100000)  # 収入は1以上100000以下である
+    income: Series[pd.Int32Dtype] = pa.Field(ge=1, le=100000, coerce=True, nullable=True)  # 収入は1以上100000以下である
     gender: Series[String] = pa.Field(isin=["male", "female"])  # 性別は"male"か"female"
     job: Series[String] = pa.Field(nullable=True)  # 職業は文字列で、欠損値（None）も許容する
     hoge: Series[Int] = pa.Field(ge=1, le=10000000, coerce=True)  # hogeは1以上10000000以下で、数値に強制変換する
--- test_pandera.py
+++ test_pandera.py
@@ -6,7 +6,7 @@ import UserSchema
 data = {
     "id": ["No_0001", "No_0002", "No_0003", "No_0004", "No_0005"],
     "age": [25, 27, 31, "45", 23],
-    "income": [50000, 62000, 10000, 87000, 92000],
+    "income": [50000, 62000, 10000, 87000, None],
     "gender": ["male", "male", "female", "male", "female"],
     "job": ["engineer", "doctor", None, "engineer", "teacher"],
     "hoge": ["22", "33", "44", "55", "66"],
